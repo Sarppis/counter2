c69acb6d597c41c9a9c68fe31450efaa4b165069
Sarpaneva Sanna <s1900151@edu.bc.fi>
2019-09-11 10:08:34 +0300

    latest version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Counter from './Counter';
 
-function App() {
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+addition = () => {
+  this.setState ({
+    count: this.state.count + 1
+  });
+};
+
+handleClick = () => {
+  this.setState ({
+    count: 0
+  });
+}
+
+handleClickAdd = () => {
+  this.setState ({
+    count: this.state.count + 1
+  });
+}
+
+handleClickRemove = () => {
+  if (this.state.count > 0){
+  this.setState ({count: this.state.count - 1
+  });
+}
+}
+
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = 'App'>
+    <h2>Class based</h2>
+    <div className = 'circle'>
+      <div className={this.state.count %2 === 0 ? "circle even" : "circle odd" } onClick={this.addition}>
+      <h1>{this.state.count}</h1>
+      </div>
     </div>
-  );
+    <div className = 'buttons'>
+      <button onClick={this.handleClick}>Reset counter</button>
+      <button onClick={this.handleClickAdd}>Add one</button>
+      <button onClick={this.handleClickRemove}>Remove one</button>
+    </div>
+    <Counter/>
+    </div>
+
+);
+}
+
 }
 
 export default App;
+
+//class based component
---
+++ src/Counter.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  //let ab =
+    //count === 0 ? 'circle neutral' ? : count %10 ===0 ? 'circle pink' : count %2 === 0 ? 'circle even' : 'circle odd'
+
+
+
+  return (
+    <div>
+    <h2>Hooks</h2>
+    <div className = 'circle'>
+      <div className={count === 0 ? 'circle neutral' : count %10 === 0 ? 'circle pink' : count %2 === 0 ? 'circle even' : 'circle odd'} onClick={() => setCount(count + 1)}><h1>{count}</h1></div>
+    </div>
+    <div className = 'buttons'>
+      <button onClick={() => setCount(0)}>Reset counter</button>
+      <button onClick={() => setCount(count+1)}>Add one</button>
+      <button onClick={count > 0 ? () => setCount(count-1) : () => setCount(0)}>Remove one</button>
+    </div>
+    </div>
+  );
+}
+
+
+
+export default Counter;
