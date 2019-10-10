df5f5b1df8567668e1219f0d119a58d908c02b74
Sarpaneva Sanna <s1900151@edu.bc.fi>
2019-10-10 11:56:41 +0300

    Updates

---
+++ precache-manifest.e4ec1c9b9e1049867f89c0570b97e305.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4dcd632d890389fc78ac5928a85759b",
+    "url": "/counter2/index.html"
+  },
+  {
+    "revision": "ee6ca6924aaa7131889b",
+    "url": "/counter2/static/css/main.b763f47a.chunk.css"
+  },
+  {
+    "revision": "0e5ea4adf592cac0b3a5",
+    "url": "/counter2/static/js/2.c5165d88.chunk.js"
+  },
+  {
+    "revision": "ee6ca6924aaa7131889b",
+    "url": "/counter2/static/js/main.a3759adc.chunk.js"
+  },
+  {
+    "revision": "e11c998a176144a7c997",
+    "url": "/counter2/static/js/runtime~main.a0177514.js"
+  }
+]);
